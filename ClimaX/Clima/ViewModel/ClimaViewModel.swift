ff08b2999ca93ae6a40369cd85796bfbc4b54003
Bruno Lopes de Mello <bruno.guitarpro@gmail.com>
2018-05-05 14:19:44 -0300

    criada ModelView para a controller Clima

---
+++ ClimaX/Clima/ViewModel/ClimaViewModel.swift
@@ -0,0 +1,74 @@
+//
+//  ClimaViewModel.swift
+//  ClimaX
+//
+//  Created by Bruno Lopes de Mello on 05/05/18.
+//  Copyright © 2018 Bruno Lopes de Mello. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+protocol ClimaViewModelProtocol {
+    func numberOfSections() -> Int
+    func numberOfRows(in section:Int) -> Int
+    func getPrevisao(in indexPath: IndexPath) -> Datum
+    func getIdCidade(cidade: String, estado: String, onComplete: @escaping (Int) -> Void, onError: @escaping (_ mensagem: String) -> Void) -> Void
+    func getClima(cidadeId: Int, onComplete: @escaping (String) -> Void, onError: @escaping (_ mensagem: String) -> Void) -> Void
+}
+
+class ClimaViewModel: ClimaViewModelProtocol {
+   
+    var previsaoTempo: [Datum] = []
+    let myToken: String = "0925e8c6873f32e349f881fa1da4564e"
+    
+    func numberOfSections() -> Int {
+        return 1
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        return previsaoTempo.count
+    }
+    
+    func getPrevisao(in indexPath: IndexPath) -> Datum {
+        return previsaoTempo[indexPath.row]
+    }
+    
+    func getIdCidade(cidade: String, estado: String, onComplete: @escaping (Int) -> Void, onError: @escaping (String) -> Void) {
+        //Request with response handling
+        request("http://apiadvisor.climatempo.com.br/api/v1/locale/city", method: .get, parameters: ["name":cidade, "state":estado, "token":myToken]).responseJSON { (response) in
+            
+            let json = JSON(response.result.value!)
+            if !json.isEmpty {
+                for object in json.arrayValue {
+                    let cityID = object["id"].intValue
+                    print("City ID: \(cityID)")
+                    onComplete(cityID)
+                }
+            } else {//consulta veio vazia []
+                onError((response.error?.localizedDescription)!)
+            }
+            
+        }
+    }
+    
+    
+    func getClima(cidadeId: Int, onComplete: @escaping (String) -> Void, onError: @escaping (String) -> Void) {
+        
+        request("http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/\(cidadeId)/days/15", method: .get, parameters: ["token":myToken]).responseJSON { (response) in
+            
+            let welcome = try? JSONDecoder().decode(Welcome.self, from: response.data!)
+            if let climas = welcome?.data {
+                let cidade = welcome?.name ?? ""
+                self.previsaoTempo = climas
+                onComplete(cidade)
+               
+            } else {
+                onError((response.error?.localizedDescription)!)
+            }
+        }
+    }
+    
+    
+}
